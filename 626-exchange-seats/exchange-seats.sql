54caa25fe3f1969513cc20c571df5d052d9d79f6
jessrobbin <jessica.robbin@theinformationlab.co.uk>
2024-12-16 15:08:54 +0000

    Time: 796 ms (5.16%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,30 +1,18 @@
 # Write your MySQL query statement below
-with all_poss as
-(select *
-, lead(id) over (order by id) as next_id
-, lag(id) over (order by id) as prev_id
-, max(id) over () as max_id
-from Seat)
-, evens as (
-    select
-    prev_id as id
-    , student
-    from all_poss
-    where mod(id, 2)=0
-)
-, odds as (
-    select
-    (case 
-        when id = max_id then id 
-        else next_id
-        end) as id
-    , student
-    from all_poss
-    where mod(id, 2)<>0
-)
 
-
-select * from odds
-union all
-select * from evens
+select
+ ( CASE 
+    when id = max_id AND mod(id,2) <> 0 then id
+    when mod(id,2) = 0 then id -1
+    when mod(id,2) <> 0 then id + 1
+    else id end
+    ) as id
+, student
+from (
+    select 
+    id
+    , student
+    , max(id) over () as max_id
+    from Seat
+) as sub
 order by id
