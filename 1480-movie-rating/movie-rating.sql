79073e78ee8b75c23b51602d2923093a5c3f729c
jessrobbin <jessica.robbin@theinformationlab.co.uk>
2024-12-16 16:51:16 +0000

    Time: 261 ms (76.81%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -2,26 +2,22 @@
 with ranked_users as (
     select 
     u.name as results
-    , rank () over (order by count(r.movie_id) desc, u.name asc) as rank
     from MovieRating R
     left join Users u on u.user_id = r.user_id
     group by u.name
+    order by count(r.movie_id) desc, u.name asc
+    limit 1
 )
 , movie_ratings as(
     select 
     m.title as results
-    , rank() over (order by avg(r.rating) desc, m.title asc) as rank
     from MovieRating r
     left join Movies m on m.movie_id = r.movie_id
     where R.created_at <= '2020-02-28' AND R.created_at >= '2020-02-01'
     group by m.title
+    order by avg(r.rating) desc, m.title asc
+    limit 1
 )
-select 
-    results
-from ranked_users
-where rank = 1
+select results from ranked_users
 union all
-select
-    results
-from movie_ratings
-where rank = 1
+select results from movie_ratings
