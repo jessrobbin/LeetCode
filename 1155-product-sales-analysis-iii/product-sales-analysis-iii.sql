fa2367aa6ea140b762d19d79a24252963ec9ca1a
jessrobbin <jessica.robbin@theinformationlab.co.uk>
2024-12-17 12:14:55 +0000

    Time: 2520 ms (8.15%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,13 +1,32 @@
 -- Write your PostgreSQL query statement below
-select
-product_id
-, year as first_year
-, quantity
-, price
-from Sales
-where (product_id, year) in (
+-- select
+-- product_id
+-- , year as first_year
+-- , quantity
+-- , price
+-- from Sales
+-- where (product_id, year) in (
+--     select
+--         product_id
+--         , min(year) over (partition by product_id) as year
+--     from Sales
+-- )
+
+with cte as (
     select
-        product_id
-        , min(year) over (partition by product_id) as year
+    product_id
+    , min(year) as first_year
     from Sales
+    group by product_id
 )
+select
+    s.product_id
+    , s.year as first_year
+    , s.quantity
+    , s.price
+from Sales s
+where (s.product_id, s.year) IN 
+    ( select
+        product_id
+        , first_year as year
+        from cte) 
